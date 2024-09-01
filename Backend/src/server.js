35c4042bf9074ba366e61c34b059894db61df220
Faizan Raza <143700173+faizanr27@users.noreply.github.com>
2024-09-01 12:28:43 +0530

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -10,8 +10,11 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://assingment-jade.vercel.app',
+}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/', signUpRoute)
