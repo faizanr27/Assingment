21709e9a702c35679b8af1c546811e2d22fe4c63
Faizan Raza <143700173+faizanr27@users.noreply.github.com>
2024-09-01 12:45:20 +0530

    Update Signup.routes.js

--- Backend/src/routes/Signup.routes.js
+++ Backend/src/routes/Signup.routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db/db');
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const upload = require('../middlewares/multerConfig');
 
 router.get('/', (req, res)=>{
     res.status(200).send("Hello this is backend for Assignment")
