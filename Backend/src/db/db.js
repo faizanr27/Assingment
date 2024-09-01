60f8fe6f28eea8ad6fb6e21b325628b5aa0bf3cf
Faizan Raza <143700173+faizanr27@users.noreply.github.com>
2024-09-01 14:05:35 +0530

    Update db.js

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -2,7 +2,29 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString:process.env.DATABASE_URL
+    connectionString:process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
   });
+
+const createTable = async () => {
+    try {
+        const query = `
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                email VARCHAR(100) UNIQUE NOT NULL,
+                resume VARCHAR(255)
+            );
+        `;
+        await pool.query(query);
+        console.log('Table ensured successfully');
+    } catch (err) {
+        console.error('Error ensuring table:', err);
+    }
+};
+
+createTable();
 
 module.exports = pool;
